4a3134eef7a32f5780aa09ae275e9520255ef5bd
Nikita Podolskiy <podolskiy06021990@gmail.com>
2026-01-25 19:25:31 +0300

    Update Class work to Git.py

--- Class work to Git.py
+++ Class work to Git.py
@@ -8,5 +8,9 @@ if __name__ == "__main__":
 if __name__ == '__main__':
     print('Аля, Улю, Чи-чи, Га-га')
 
+<<<<<<< Updated upstream
+    print("Изменил  development")
+>>>>>>> Stashed changes
+=======
     print("Изменил  development")
 >>>>>>> Stashed changes
