8743894686a4f09c21f0fb76505265b64596a23d
Nikita Podolskiy <podolskiy06021990@gmail.com>
2026-01-27 09:12:20 +0300

    Update Class work to Git.py

--- Class work to Git.py
+++ Class work to Git.py
@@ -3,10 +3,4 @@ def greet(name):
     print(f"Hello, {name}!")
 if __name__ == "__main__":
     greet("Alice")
-    
-=======
-if __name__ == '__main__':
-    print('Аля, Улю, Чи-чи, Га-га')
 
-    print("Изменил  development")
->>>>>>> Stashed changes
